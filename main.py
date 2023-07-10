3fd5d016db31769f83a8cabaeada1651be12cc68
Moein Aghamirzaei <moein.6556@gmail.com>
2023-07-10 04:25:23 +0330

    save

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pyautogui
 import datetime
 import time
 
-START_TIME = "03:56:15.507"
+START_TIME = "04:27:56:15.507"
 
 AFTER_CLICK = [0.500, 0.400, 0.444]
 # کلیک های بعد به ثانیه از چپ به راست
+
+
+print(f'start at {START_TIME}')
 
 
 def my_function():
